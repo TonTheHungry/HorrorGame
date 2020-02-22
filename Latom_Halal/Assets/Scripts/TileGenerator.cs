3944b0d27a33d8e200bf515966dac25ea246a3d1
Ton-10 <taveon30@yahoo.com>
2020-02-21 23:24:45 -0600

    added left and right path logic to tile generator script

--- Latom_Halal/Assets/Scripts/TileGenerator.cs
+++ Latom_Halal/Assets/Scripts/TileGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TileGenerator : MonoBehaviour
 {
@@ -9,16 +7,27 @@ public class TileGenerator : MonoBehaviour
     private Vector3 plrPosition;
     private Vector3 newPoint;
     private GameObject generatedTile;
-    enum GeneratedType { North, East, South, West };
-
+    enum GeneratedType { Left, Right, Straight, Big };
+    GeneratedType previousGen;
+    GeneratedType currentGen;
+    private int r;
+    private int type;
 
     // Start is called before the first frame update
     void Start()
     {
+        r = Random.Range(0, 3);
+        if (r == 0)
+            currentGen = GeneratedType.Left;
+        if (r == 1)
+            currentGen = GeneratedType.Right;
+        if (r == 2)
+            currentGen = GeneratedType.Straight;
+
         plrPosition = Node.transform.position;
         newPoint = plrPosition + new Vector3(0, 0, 7);
         transform.position = newPoint;
-        generatedTile = Instantiate(Tiles[0], newPoint, Quaternion.identity);
+        generatedTile = Instantiate(Tiles[r], newPoint, Quaternion.identity);
         print(newPoint);
     }
 
@@ -28,11 +37,48 @@ public class TileGenerator : MonoBehaviour
         plrPosition = Node.transform.position;
         if ((plrPosition - newPoint).sqrMagnitude < 20000)
         {
+            previousGen = currentGen;
+            r = Random.Range(0, 3);
+            setTileType(r);
             newPoint = newPoint + new Vector3(0, 0, 7);
-            print("newtile spawn");
-            generatedTile = Instantiate(Tiles[0], newPoint, Quaternion.identity);
+            generatedTile = Instantiate(Tiles[type], newPoint, Quaternion.identity);
             transform.position = newPoint;
         }
-        
+
+    }
+    private void setTileType(int num)
+    {
+        if (num == 0)
+        {
+            if (previousGen != GeneratedType.Left)
+            {
+                currentGen = GeneratedType.Left;
+                type = num;
+            }
+            else
+            {
+                currentGen = GeneratedType.Right;
+                type = 1;
+            }
+
+        }
+        if (num == 1)
+        {
+            if (previousGen != GeneratedType.Right)
+            {
+                currentGen = GeneratedType.Right;
+                type = r;
+            }
+            else
+            {
+                currentGen = GeneratedType.Left;
+                type = 0;
+            }
+        }
+        if (num == 2)
+        {
+            currentGen = GeneratedType.Straight;
+            type = r;
+        }
     }
 }
