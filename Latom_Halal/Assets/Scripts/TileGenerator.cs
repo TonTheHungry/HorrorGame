7a6097a7263d5c4ae4e1efb31c1193e312d709fd
Ton-10 <46650315+Ton-10@users.noreply.github.com>
2020-02-26 11:24:45 -0600

    Added wall generation & logic to tile generator

--- Latom_Halal/Assets/Scripts/TileGenerator.cs
+++ Latom_Halal/Assets/Scripts/TileGenerator.cs
@@ -3,13 +3,12 @@
 public class TileGenerator : MonoBehaviour
 {
     public GameObject Node;
-    public GameObject[] Tiles;
+    public GameObject Tile;
     private Vector3 plrPosition;
     private Vector3 newPoint;
     private GameObject generatedTile;
     GameObject previousTile;
-    private int r;
-    private int left, right;
+    private int left, right, room, r;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +16,8 @@ public class TileGenerator : MonoBehaviour
         plrPosition = Node.transform.position;
         newPoint = plrPosition + new Vector3(0, 0, 10);
         transform.position = newPoint;
-        generatedTile = Instantiate(Tiles[2], newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+        generatedTile = Instantiate(Tile, newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+        generatedTile.name = "Straight";
         previousTile = generatedTile;
 
     }
@@ -35,38 +35,56 @@ public class TileGenerator : MonoBehaviour
     }
     private void makeTileType(int num)
     {
+        //generate left tile
         if (num == 0 && right <= 0)
         {
             left = 3;
-            if (previousTile.transform.Find("SideL") != null)
+            if (previousTile.name == "Straight")
             {
                 GameObject side = previousTile.transform.Find("SideL").gameObject;
                 side.transform.Rotate(new Vector3(0, 0, 90));
                 side.transform.localPosition = new Vector3(-5, 5, 0);
             }
                 newPoint = newPoint + new Vector3(-10, 0, 0);
-                generatedTile = Instantiate(Tiles[num], newPoint, Quaternion.identity);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
+                generatedTile.name = "Left";
                 transform.position = newPoint;
         }
+        //generate right tile
         if (num == 1 && left <= 0)
         {
             right = 3;
-            if (previousTile.transform.Find("SideR") != null)
+            if (previousTile.name == "Straight")
             {
                 GameObject side = previousTile.transform.Find("SideR").gameObject;
                 side.transform.Rotate(new Vector3(0, 0, 90));
                 side.transform.localPosition = new Vector3(-5, 5, 0);
             }
                 newPoint = newPoint + new Vector3(10, 0, 0);
-                generatedTile = Instantiate(Tiles[num], newPoint, Quaternion.identity);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
+                generatedTile.name = "Right";
                 transform.position = newPoint;
         }
+        //generate straight tile
         if (num == 2)
         {
             left--;
             right--;
+            if (previousTile.name == "Left")
+            {
+                GameObject side = previousTile.transform.Find("SideR").gameObject;
+                side.transform.Rotate(new Vector3(0, 0, 90));
+                side.transform.localPosition = new Vector3(-5, 5, 0);
+            }
+            if (previousTile.name == "Right")
+            {
+                GameObject side = previousTile.transform.Find("SideR").gameObject;
+                side.transform.Rotate(new Vector3(0, 0, 90));
+                side.transform.localPosition = new Vector3(5, 5, 0);
+            }
             newPoint = newPoint + new Vector3(0, 0, 10);
-            generatedTile = Instantiate(Tiles[num], newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+            generatedTile = Instantiate(Tile, newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+            generatedTile.name = "Straight";
             transform.position = newPoint;
         }
         previousTile = generatedTile;
