953415fd365ca799662ea28d0a54efbdc3c96c1f
theresagill <36672875+theresagill@users.noreply.github.com>
2020-02-27 12:49:59 -0600

    Added playermovement and mouselook.cs to character scene

---
+++ Latom_Halal/Assets/Scripts/MovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed* Input.GetAxis("Horizontal") * Time.deltaTime, 0f,
+            moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        
+    }
+}
