e6895d206e07b8fc5e745842b3c2beb4ff532f39
Ton-10 <taveon30@yahoo.com>
2020-02-26 22:41:02 -0600

    Added room generation for generator script

--- Latom_Halal/Assets/Scripts/TileGenerator.cs
+++ Latom_Halal/Assets/Scripts/TileGenerator.cs
@@ -4,15 +4,17 @@ public class TileGenerator : MonoBehaviour
 {
     public GameObject Node;
     public GameObject Tile;
+    public GameObject Room;
     private Vector3 plrPosition;
     private Vector3 newPoint;
     private GameObject generatedTile;
     GameObject previousTile;
-    private int left, right, room, r;
+    private int left, right, room, r,t;
 
     // Start is called before the first frame update
     void Start()
     {
+        room = 20;
         plrPosition = Node.transform.position;
         newPoint = plrPosition + new Vector3(0, 0, 10);
         transform.position = newPoint;
@@ -28,13 +30,52 @@ public class TileGenerator : MonoBehaviour
         plrPosition = Node.transform.position;
         if ((plrPosition - newPoint).sqrMagnitude < 20000)
         {
-            r = Random.Range(0, 3);
-            makeTileType(r);
+            t = Random.Range(0, 3);
+            r = Random.Range(0, 5);
+            makeRoomType(r);
+            makeTileType(t);
         }
 
     }
+    private void makeRoomType(int num)
+    {
+        //generate room tile
+        if (num == 0 && room <= 0)
+        {
+           if (previousTile.name == "Straight")
+           {
+                room = 20;
+                newPoint = newPoint + new Vector3(0, 0, 25);
+                generatedTile = Instantiate(Room, newPoint, Quaternion.identity);
+                generatedTile.name = "Room";
+                transform.position = newPoint;
+                previousTile = generatedTile;
+            }
+           if (previousTile.name == "Right")
+           {
+                room = 20;
+                newPoint = newPoint + new Vector3(25, 0, 0);
+                generatedTile = Instantiate(Room, newPoint, Quaternion.identity);
+                generatedTile.name = "Room";
+                transform.position = newPoint;
+                previousTile = generatedTile;
+            }
+           if (previousTile.name == "Left")
+           {
+                room = 20;
+                newPoint = newPoint + new Vector3(-25, 0, 0);
+                generatedTile = Instantiate(Room, newPoint, Quaternion.identity);
+                generatedTile.name = "Room";
+                transform.position = newPoint;
+                previousTile = generatedTile;
+
+            }
+        }
+      
+    }
     private void makeTileType(int num)
     {
+        room--;
         //generate left tile
         if (num == 0 && right <= 0)
         {
@@ -45,10 +86,21 @@ public class TileGenerator : MonoBehaviour
                 side.transform.Rotate(new Vector3(0, 0, 90));
                 side.transform.localPosition = new Vector3(-5, 5, 0);
             }
+            if (previousTile.name == "Room")
+            {
+                newPoint = newPoint + new Vector3(-25, 0, 0);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
+                generatedTile.name = "Left";
+                transform.position = newPoint;
+                //new generator here
+            }
+            else
+            {
                 newPoint = newPoint + new Vector3(-10, 0, 0);
                 generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
                 generatedTile.name = "Left";
                 transform.position = newPoint;
+            }
         }
         //generate right tile
         if (num == 1 && left <= 0)
@@ -60,10 +112,21 @@ public class TileGenerator : MonoBehaviour
                 side.transform.Rotate(new Vector3(0, 0, 90));
                 side.transform.localPosition = new Vector3(-5, 5, 0);
             }
+            if (previousTile.name == "Room")
+            {
+                newPoint = newPoint + new Vector3(25, 0, 0);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
+                generatedTile.name = "Right";
+                transform.position = newPoint;
+                //new generator here
+            }
+            else
+            {
                 newPoint = newPoint + new Vector3(10, 0, 0);
                 generatedTile = Instantiate(Tile, newPoint, Quaternion.identity);
                 generatedTile.name = "Right";
                 transform.position = newPoint;
+            }
         }
         //generate straight tile
         if (num == 2)
@@ -82,10 +145,21 @@ public class TileGenerator : MonoBehaviour
                 side.transform.Rotate(new Vector3(0, 0, 90));
                 side.transform.localPosition = new Vector3(5, 5, 0);
             }
-            newPoint = newPoint + new Vector3(0, 0, 10);
-            generatedTile = Instantiate(Tile, newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
-            generatedTile.name = "Straight";
-            transform.position = newPoint;
+            if (previousTile.name == "Room")
+            {
+                newPoint = newPoint + new Vector3(0, 0, 25);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+                generatedTile.name = "Straight";
+                transform.position = newPoint;
+                //new generator here
+            }
+            else
+            {
+                newPoint = newPoint + new Vector3(0, 0, 10);
+                generatedTile = Instantiate(Tile, newPoint, Quaternion.Euler(new Vector3(0, 90, 0)));
+                generatedTile.name = "Straight";
+                transform.position = newPoint;
+            }
         }
         previousTile = generatedTile;
     }
