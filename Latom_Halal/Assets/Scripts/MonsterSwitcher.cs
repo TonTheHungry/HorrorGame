5cc1f90118c44113facb68fa2dcd2a631ddc8373
Ian <42393430+Ianomia@users.noreply.github.com>
2020-03-16 22:13:04 -0500

    Add files via upload
    These are a few scripts for the monster. It'll allow the monster to follow and switch between different models.

---
+++ Latom_Halal/Assets/Scripts/MonsterSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSwitcher : MonoBehaviour
+{
+    public GameObject switch1;
+    public GameObject switch2;
+    public GameObject switch3;
+    public int monNum = 0;
+    private GameObject current;
+
+    void Start()
+    {
+        current = Instantiate(switch1, transform.position, transform.rotation);
+        current.transform.parent = transform;
+    }
+
+   
+    void Update()
+    {
+        if (monNum == 1)
+        {
+            GameObject thisobject = Instantiate(switch2, transform.position, transform.rotation) as
+                GameObject;
+            Destroy(current);
+            thisobject.transform.parent = transform;
+            current = thisobject;
+        }
+        else if (monNum == 2)
+        {
+            GameObject thisobject = Instantiate(switch3, transform.position, transform.rotation) as
+                GameObject;
+            Destroy(current);
+            thisobject.transform.parent = transform;
+            current = thisobject;
+        }
+        else
+        {
+            GameObject thisobject = Instantiate(switch1, transform.position, transform.rotation) as
+    GameObject;
+            Destroy(current);
+            thisobject.transform.parent = transform;
+            current = thisobject;
+        }
+    }
+}
