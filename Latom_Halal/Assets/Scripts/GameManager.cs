25f5d389d08d430bb7c2d434d625be53d89ad434
Ton-10 <taveon30@yahoo.com>
2020-03-17 18:20:00 -0500

    modified rooms

---
+++ Latom_Halal/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlayerPrefab, GeneratorPrefab, MonsterPrefab;
+    public Material WallMat;
+    private GameObject playerObj,monsterObj,generatorObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Start!");
+        GameObject map = Instantiate(new GameObject(), new Vector3(0, 0, 0), Quaternion.identity);
+        map.name = "Map Container";
+        playerObj = Instantiate(PlayerPrefab, new Vector3(0,0,0), Quaternion.identity);
+        generatorObj = Instantiate(GeneratorPrefab, new Vector3(0,-5,0), Quaternion.identity);
+        generatorObj.GetComponent<TileGenerator>().MapContainer = map;
+        generatorObj.GetComponent<TileGenerator>().Node = playerObj;
+        GameObject cap = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cap.transform.position = new Vector3(0, 0, -6);
+        cap.GetComponent<Renderer>().material = WallMat;
+        cap.transform.localScale = new Vector3(10, 10, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
