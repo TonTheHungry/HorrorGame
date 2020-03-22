3493a025d8264821d473839a804dad026c1befa4
Ian <RPGMasterIan@aol.com>
2020-03-22 18:56:29 -0500

    Monsters motions added. Scripts too.

---
+++ Latom_Halal/Assets/Scripts/ForcedAnimate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcedAnimate : MonoBehaviour
+{
+    public Animator animator;
+    public Vector3 point1;
+    public Vector3 point2;
+    private Vector3 distance;
+    private Vector3 point3;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        point1 = transform.position;
+        point2 = transform.position;
+        distance = point2 - point2;
+    }
+
+    void Update()
+    {
+        point2 = transform.position;
+        distance = point3 - point2;
+
+        // An if to change the animation bool & one to stop it. 
+        if (point2 != point1)
+        {
+            animator.SetBool("IsWalking", true);
+            point3 = transform.position;
+        }
+
+        if (distance.x == 0 && distance.y == 0 && distance.z == 0)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("CreepyRun", false);
+            point1 = point2;
+        } 
+    }
+}
