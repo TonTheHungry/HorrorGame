b6af949822e188ecf55ec7226ef76ba8ccdbc0b1
Ian <RPGMasterIan@aol.com>
2020-04-04 17:35:48 -0500

    Just Pulling

---
+++ Latom_Halal/Assets/Scripts/HealthbarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarController : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public MonsterHealth monsterHealth;
+    public Animator anim;
+
+   
+    void Start()
+    {
+        currentHealth = maxHealth;
+        monsterHealth = GetComponent<MonsterHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+       // anim.Play("Hit");
+        currentHealth -= damage;
+        monsterHealth.HPSlider.value = currentHealth;
+
+        if (monsterHealth.HPSlider.value <= 0)
+        {
+            anim.Play("Death");
+        }
+
+    }
+}
